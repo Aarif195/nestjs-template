6126b8918ad711aacc0976d30f7ae4fa64832c55
Aarif <adebayoabdulazeez195@gmail.com>
2026-02-26 15:02:10 +0100

    resolve Prisma initialization and DB connection issues

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,13 +1,22 @@
-import { PrismaClient } from '@/generated/prisma/client';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+      console.log('Prisma connected');
+
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+  }
+
+  async testDb() {
+    return await this.$queryRaw`SELECT 1`;
   }
 }
