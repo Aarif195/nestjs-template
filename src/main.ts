56584a5af23ee14f8f8c4bd285e220e1b1742e77
Aarif <adebayoabdulazeez195@gmail.com>
2026-02-26 15:09:07 +0100

    initialize swagger documentation at /api/docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Student Welfare Platform API')
+    .setDescription('The API documentation for the Student Welfare Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
